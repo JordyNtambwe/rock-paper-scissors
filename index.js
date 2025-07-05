776742a30e3221f153e13dda806a75bc6823e5b9
JordyNtambwe <jordyntambwe@gmail.com>
2025-07-04 19:42:20 -0600

    created a function that increments the round winner’s score and logs a winner announcement

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function getComputerChoice () {
+
   const random = Math.random();
   if (random <= 0.33) {
     return "rock";
@@ -7,9 +8,11 @@ function getComputerChoice () {
   } else {
     return "scissors";
   };
+
 };
 
 function getHumanChoice () {
+
   const choice = prompt ("Enter your choice: Rock, Paper, or Scissors?");
 
   if (choice === "rock") {
@@ -19,9 +22,31 @@ function getHumanChoice () {
   } else {
     return "scissors";
   };
+
 };
 
+ let humanScore = 0;
+ let computerScore = 0;
 
-const humanScore = 0;
-const computerScore = 0;
+function playRound(humanChoice, computerChoice) {
 
+  humanChoice = humanChoice.toLowerCase();
+
+  if (humanChoice === computerChoice) {
+    return "It's a tie!";
+  } else if (
+    humanChoice === "rock" && computerChoice === "scissors" ||
+    humanChoice === "paper" && computerChoice === "rock" ||
+    humanChoice === "scissors" && computerChoice === "paper"
+  ) {
+    humanScore++;
+    return "Good job! One point goes to you!" 
+  } else {
+    computerScore++;
+    return "That's too bad! One point goes to your opponent!"
+  };
+
+};
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
