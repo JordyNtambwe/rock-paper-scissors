503c62efc9270e50320e347a2485ed74846267c7
JordyNtambwe <jordyntambwe@gmail.com>
2026-02-06 15:25:07 -0700

    created 2 variables and placed them in the global scope to keep track of the players score

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+// These variables keeps track of the players score
+let humanScore = 0;
+let computerScore = 0;
 // This functions get the computer to randomly return one of the string values: 'rock', 'paper', or 'scissors
 function getComputerChoice() {
   let choice = Math.random()
@@ -12,4 +15,4 @@ function getComputerChoice() {
 // This functions prompts the user to choose between 'rock', 'paper', or 'scissors' and return their choice
 function getHumanChoice() {
   return prompt('Pick your choice: Rock, Paper, or Scissors?');
-}
+};
