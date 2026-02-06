0ed07f40dfed989f877b400b033408e948bc5aca
JordyNtambwe <jordyntambwe@gmail.com>
2026-02-06 16:18:30 -0700

    capitalized the first letter of the choices in the 'getComputerChoice()' function for grammatical reasons
    edited the 'getHumanChoice()' function to capitalize the first letter of each choice while leaving the rest lower case
    
    created the 'playRound' function that consists of the logic of taking both the human and computer choices, play a single round, and increment the round winner's score and log a winner in the console

--- javascript.js
+++ javascript.js
@@ -1,18 +1,42 @@
-// These variables keeps track of the players score
-let humanScore = 0;
-let computerScore = 0;
 // This functions get the computer to randomly return one of the string values: 'rock', 'paper', or 'scissors
 function getComputerChoice() {
   let choice = Math.random()
   if (choice <= 1/3) {
-    return 'rock';
+    return 'Rock';
   } else if (choice < 2/3) {
-    return 'paper';
+    return 'Paper';
   } else {
-    return 'scissors';
+    return 'Scissors';
   };
 };
 // This functions prompts the user to choose between 'rock', 'paper', or 'scissors' and return their choice
 function getHumanChoice() {
-  return prompt('Pick your choice: Rock, Paper, or Scissors?');
+  let question = prompt('Pick your choice: Rock, Paper, or Scissors?')
+  let userChoice = question[0].toUpperCase() + question.slice(1).toLowerCase();
+  return userChoice;
 };
+
+// These variables keeps track of the players score
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound (humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log(`It's a tie! Both chose ${humanChoice}.`);
+  } else if (
+    humanChoice === 'Rock' && computerChoice === 'Scissors' ||
+    humanChoice === 'Paper' && computerChoice === 'Rock' ||
+    humanChoice === 'Scissors' && computerChoice == 'Paper'
+  ) {
+    humanScore++;
+    console.log(`You won this round! ${humanChoice} beats ${computerChoice}.`);
+  } else {
+    computerScore++;
+    console.log(`You lost this round! ${computerChoice} beats ${humanChoice}.`);
+  };
+};
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
