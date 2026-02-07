4a082c048ef7ac1aca8e0735dc9e5dd80ab3290d
JordyNtambwe <jordyntambwe@gmail.com>
2026-02-07 14:22:44 -0700

    created the 'playGame' function to call on the 'playRound' function to play 5 rounds, keeps track of the scores, and declares a winner at the end of the game

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 // This functions get the computer to randomly return one of the string values: 'rock', 'paper', or 'scissors
 function getComputerChoice() {
   let choice = Math.random()
-  if (choice <= 1/3) {
+  if (choice <= 1 / 3) {
     return 'Rock';
-  } else if (choice < 2/3) {
+  } else if (choice < 2 / 3) {
     return 'Paper';
   } else {
     return 'Scissors';
@@ -15,27 +15,41 @@ function getHumanChoice() {
   let userChoice = question[0].toUpperCase() + question.slice(1).toLowerCase();
   return userChoice;
 };
-
-// These variables keeps track of the players score
-let humanScore = 0;
-let computerScore = 0;
-// This function takes the human and computer choice, play a single round, increment the round winner's score, and log the winner in the console
-function playRound (humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
-    console.log(`It's a tie! Both chose ${humanChoice}.`);
-  } else if (
-    humanChoice === 'Rock' && computerChoice === 'Scissors' ||
-    humanChoice === 'Paper' && computerChoice === 'Rock' ||
-    humanChoice === 'Scissors' && computerChoice == 'Paper'
-  ) {
-    humanScore++;
-    console.log(`You won this round! ${humanChoice} beats ${computerChoice}.`);
+// This function calls on the 'playRound' function to play 5 rounds, keeps track of the scores, and declares a winner at the end of the game
+function playGame() {
+  // These variables keeps track of the players score
+  let humanScore = 0;
+  let computerScore = 0;
+  // This function takes the human and computer choice, play a single round, increment the round winner's score, and log the winner in the console each round
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      console.log(`It's a tie! Both chose ${humanChoice}. The score is stil ${humanScore} - ${computerScore}.`);
+    } else if (
+      humanChoice === 'Rock' && computerChoice === 'Scissors' ||
+      humanChoice === 'Paper' && computerChoice === 'Rock' ||
+      humanChoice === 'Scissors' && computerChoice == 'Paper'
+    ) {
+      humanScore++;
+      console.log(`You won this round! ${humanChoice} beats ${computerChoice}. The score is now ${humanScore} - ${computerScore}.`);
+    } else {
+      computerScore++;
+      console.log(`You lost this round! ${computerChoice} beats ${humanChoice}. The score is now ${computerScore} - ${humanScore}`);
+    };
+  };
+  //This is responsible for the 'playGame' function to only play 5 rounds
+  for (let i = 1; i <= 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+  };
+  //This is responsible for the winners declaration at the end of the 5 rounds
+  if (humanScore === computerScore) {
+    console.log(`It's game over! It's a tie! The final score is ${humanScore} - ${computerScore}`);
+  } else if (humanScore > computerScore) {
+    console.log(`It's game over! You won! The final score is ${humanScore} - ${computerScore}`);
   } else {
-    computerScore++;
-    console.log(`You lost this round! ${computerChoice} beats ${humanChoice}.`);
+    console.log(`It's game over! You lost! The final score is ${computerScore} - ${humanScore}`);
   };
 };
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+playGame();
