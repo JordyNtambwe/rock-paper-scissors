400309e76e0e98e13fca996c03e96de7e5953ba4
JordyNtambwe <jordyntambwe@gmail.com>
2026-02-21 17:38:30 -0700

    changed all of the console.log into a DOM method

--- javascript.js
+++ javascript.js
@@ -22,19 +22,22 @@ function playGame() {
   let computerScore = 0;
   // This function takes the human and computer choice, play a single round, increment the round winner's score, and log the winner in the console each round
   function playRound(humanChoice, computerChoice) {
+    const results = document.querySelector('#results');
+    const content = document.createElement('div');
     if (humanChoice === computerChoice) {
-      console.log(`It's a tie! Both chose ${humanChoice}. The score is stil ${humanScore} - ${computerScore}.`);
+      content.textContent = `It's a tie! Both chose ${humanChoice}. The score is stil ${humanScore} - ${computerScore}.`;
     } else if (
       humanChoice === 'Rock' && computerChoice === 'Scissors' ||
       humanChoice === 'Paper' && computerChoice === 'Rock' ||
       humanChoice === 'Scissors' && computerChoice == 'Paper'
     ) {
       humanScore++;
-      console.log(`You won this round! ${humanChoice} beats ${computerChoice}. The score is now ${humanScore} - ${computerScore}.`);
+      content.textContent = `You won this round! ${humanChoice} beats ${computerChoice}. The score is now ${humanScore} - ${computerScore}.`;
     } else {
       computerScore++;
-      console.log(`You lost this round! ${computerChoice} beats ${humanChoice}. The score is now ${computerScore} - ${humanScore}`);
+      content.textContent = `You lost this round! ${computerChoice} beats ${humanChoice}. The score is now ${computerScore} - ${humanScore}`;
     };
+    results.appendChild(content);
   };
   //This is responsible for the 'playGame' function to only play 5 rounds
   /* for (let i = 1; i <= 5; i++) {
